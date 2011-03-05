4ca02755b3ffe3d2e43c12c0cabd9d3af0a7610e
前田 康行 <maeda@Air.local>
2011-03-05 16:56:19 +0900

    HTMLを出力するように変更

--- highlight/src/script/HtmlBuildTest.scala
+++ highlight/src/script/HtmlBuildTest.scala
@@ -1,25 +1,32 @@
+import lyrical.highlighter._
+
+val str = Highlighter.buildHtml("""
 package lyrical.highlighter
 
 object Highlighter {
   
   import ScalaTokenParsers._
   
-  def highlight(srcText: String):String = highlight("", srcText)
+//  def highlight(srcText: String):String = highlight("", srcText)
 
   def highlight(filename: String, srcText: String):String = ScalaTokenParsers.parseAll(scalaParser, srcText) match {
     case Success(x, _) => x
     case Failure(msg, _) => msg
     case Error(msg, _) => msg
   }
-
+/*
   def buildHtml(srcText: String): String = buildHtml("", srcText)
 
   def buildHtml(filename: String, srcText: String): String = {
-    val builder = new HtmlBuilder with NormalCssBuilder with BodyBuilder {
+    val builder = new HtmlBuilder with CssBuilder with BodyBuilder {
       override def body = highlight(filename, srcText)
     }
 
     builder.build
   }
-
+*/
 }
+""")
+
+
+//println(str)
