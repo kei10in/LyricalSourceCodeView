89e6faa67f89ac961b6faee56ff2b0fb2484a342
kei10in <kei10in@gmail.com>
2011-03-05 23:16:30 +0900

    ScalaTest のバージョンを 1.1 に変更

--- highlight/project/build/LyricalHighlighter.scala
+++ highlight/project/build/LyricalHighlighter.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class LyricalHighlighterProject(info: ProjectInfo) extends DefaultProject(info) {
-  val scalaTest = "org.scalatest" % "scalatest" % "1.2"
+  val scalaTest = "org.scalatest" % "scalatest" % "1.1"
 }
